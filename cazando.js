00ceb70335463853ad985b02307416a02364e72e
XxRogeoxX <ronaldsan3.3@gmail.com>
2026-04-11 22:54:53 -0500

    feat . CHANGE OF VARIABLES AND CONSTANTS, UPDATED POINT 18

--- cazando.js
+++ cazando.js
@@ -1,9 +1,31 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
-function dibujarRectangulo(){
-    let anchoGato=100;
-    let altoGato=100;
-    ctx.fillStyle ="#27B8AB";
-    ctx.fillRect(canvas.width/4,canvas.height/4,canvas.width*2/4,canvas.height*2/4);
+let gatoX = 0;
+let gatoY = 0;
+
+let comidaX = 0;
+let comidaY = 0;
+
+const ALTO_GATO = 50;
+const ANCHO_GATO = 80;
+
+const ALTO_COMIDA = 25;
+const ANCHO_COMIDA = 25;
+
+const ANCHO = canvas.width / 2 - ANCHO_GATO / 2;
+const ALTO = canvas.height / 2 - ALTO_GATO / 2;
+
+
+function graficarGato(){
+    ctx.fillStyle ="#db8707";
+    ctx.fillRect(ANCHO,ALTO,ANCHO_GATO,ALTO_GATO);
+}
+function graficarComida(){
+    ctx.fillStyle ="#27b83f";
+    ctx.fillRect(canvas.width - ANCHO_COMIDA,canvas.height - ALTO_COMIDA,ANCHO_COMIDA,ALTO_COMIDA);
+}
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
 }
