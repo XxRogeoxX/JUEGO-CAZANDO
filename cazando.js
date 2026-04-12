f4b155aa079e50ee3926000f1c3c3ec5b1f99472
XxRogeoxX <ronaldsan3.3@gmail.com>
2026-04-11 23:37:19 -0500

    feat : Create a function to graficarRectangulo and update functions

--- cazando.js
+++ cazando.js
@@ -16,14 +16,15 @@ const ANCHO_COMIDA = 25;
 const ANCHO = canvas.width / 2 - ANCHO_GATO / 2;
 const ALTO = canvas.height / 2 - ALTO_GATO / 2;
 
-
+function graficarRectangulo(x,y,ancho,alto,color){
+    ctx.fillStyle = color
+    ctx.fillRect(x,y,ancho,alto)
+}
 function graficarGato(){
-    ctx.fillStyle ="#db8707";
-    ctx.fillRect(ANCHO,ALTO,ANCHO_GATO,ALTO_GATO);
+    graficarRectangulo(ANCHO,ALTO,ANCHO_GATO,ALTO_GATO,"blue");
 }
 function graficarComida(){
-    ctx.fillStyle ="#27b83f";
-    ctx.fillRect(canvas.width - ANCHO_COMIDA,canvas.height - ALTO_COMIDA,ANCHO_COMIDA,ALTO_COMIDA);
+    graficarRectangulo(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA,"green");
 }
 function iniciarJuego(){
     graficarGato();
